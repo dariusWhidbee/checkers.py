c7369e1e1cc02cabc95f4da91ccd88153a796864
csPython <cspython@AD25TF04NGF1J-78.local>
2019-03-14 14:00:54 -0400

    added users input for the size of the checker board and the color

--- checkers.py
+++ checkers.py
@@ -5,9 +5,12 @@ def draw_sq(sX, sY, color, size, win):
                        Point(sX + size, sY + size))
     square.setFill(color)
     square.draw(chWin)
-    
-sqSz = 50
-sqCol = "red"
+
+print("""Hello user. This program creates a 8x8 checker board. Type the size you'd
+want the checker board to be and the color.""")
+print()
+sqSz = int(input("size > "))
+color = str(input("color > "))
 
 
 
@@ -17,7 +20,7 @@ chWin.setCoords(0,0, sqSz * 10, sqSz * 10)
 for m in range(8):
     for i in range(8):
         if (i + m) % 2 == 0:
-            sqCol = "red"
+            sqCol = color
         else:
             sqCol = "black"
         draw_sq(sqSz * (i + 1), sqSz * (m + 1) , sqCol, sqSz, chWin)
